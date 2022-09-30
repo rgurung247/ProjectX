4682c37b30a85301348d6ef77b925e4ee53f0605
Rabi Gurung <info@xybernetics.com>
2022-09-30 22:52:54 +0600

    Add files via upload

---
+++ Xamarin/0001 Navigate Between Pages And Pass Values/App4/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NavToPage1_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.Properties["SomeText"] = PassValue.Text;
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
